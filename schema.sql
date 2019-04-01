f976e32660884dde97f570f2b4bb4d4476de56de
SNORISS <samuel.norris@marquette.edu>
2019-04-01 13:17:41 -0500

    updated schema

--- schema.sql
+++ schema.sql
@@ -16,3 +16,18 @@ stock_quantity int(11),
 PRIMARY KEY (item_id)
 
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("DOOM", "Games", 60.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Quake", "Games", 59.99, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("CS:GO", "Games", 10.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("POE", "Games", 40.00, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("DOTA", "Games", 1.00, 50);
